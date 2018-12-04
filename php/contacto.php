63e33765b7864354947b7b7cfb6a66d6b1735e64
thekaki <samuel.kaki.10@gmail.com>
2018-12-04 22:24:55 +0100

    primer commit de mi sitio web

---
+++ php/contacto.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>Gracias</title>
+ </head>
+ <body>
+ 
+<?php
+echo 'Gracias '; $nombre = $_POST['nombre']; echo $nombre;
+echo ' ';
+$apellidos = $_POST['apellidos']; echo $apellidos; echo ' ,su email ha sido recibido y sera respondido lo antes posible.';
+ ?>
+
+ </body>
+</html>
